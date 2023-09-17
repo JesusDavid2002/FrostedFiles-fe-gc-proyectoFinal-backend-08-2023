bcd678bb2bcf72f9361576f9630974350c46c2ad
GCMrybakin <135844963+GCMrybakin@users.noreply.github.com>
2023-09-17 16:18:20 +0200

    Update Users.java

--- src/main/java/com/example/proyecto/dto/Users.java
+++ src/main/java/com/example/proyecto/dto/Users.java
@@ -13,7 +13,9 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
